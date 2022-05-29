4286487642a8dc2a6ccc2b99831491e2a54551e6
Tulio Faria <tuliofaria@gmail.com>
2022-05-29 19:56:25 -0300

    adding initial template

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,13 @@
+import Footer from '~/components/Footer'
+import Header from '~/components/Header'
+import Hero from '~/components/Hero'
+
 export default function Index() {
   return (
     <div>
-      <h1>Welcome to Learn49</h1>
+      <Header />
+      <Hero />
+      <Footer />
     </div>
   )
 }
